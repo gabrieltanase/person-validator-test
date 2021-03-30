4a19d1bc46f6ee2cf76555a4cb7b17fa70b4d56a
Gabriel Tanase <gabycj88@gmail.com>
2021-03-31 01:52:57 +0200

    Person details object validator

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,72 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <PersonDetails v-if="person" :person="person"/>
+    <p v-else>Person data is not matching the Schema!</p>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import _ from 'lodash';
+import PersonDetails from './components/PersonDetails.vue'
+
+import { IPersonDetails } from './models/person';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    PersonDetails
+  },
+  data() {
+    return { 
+      person: this.validatePersonData(this.fetchDataFromApi()),
+      message: null 
+    }
+  },
+  methods: {
+    /**  
+     * Fetch the data from backend (dummy data)
+    */
+    fetchDataFromApi() {
+      return {
+        name: 'James', 
+        age: 25,
+        siblings: ['Johnnathan'],
+        metaData: {},
+        active: true,
+      }
+    },
+  
+    /** 
+     * Validates person data that comes from API for consistancy 
+     * @param data - person details 
+     * @returns Object - personDetails matched data against person details schema
+     * @returns unmatching data message
+    */
+    validatePersonData(data) {
+      if (!data) {
+        return;
+      }
+      // Define the person schema
+      const personSchema = IPersonDetails
+
+      /** Iterate data and check each property against the schema types 
+       * stops the iteration when
+      */
+      const check = _.every(data, (value, key) => {
+        /**
+         * exception for Array
+         * check if the value is an array against same property from schema object 
+         */ 
+        if (_.isArray(value) && personSchema[key] === 'array') {
+          return true;
+        }
+        return typeof value === personSchema[key];
+      });
+      
+      console.log(check);
+      return data;
+    }
   }
 }
 </script>
@@ -21,7 +76,6 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
 }
---
+++ src/components/PersonDetails.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="wrapper">
+    <div class="details">
+      <h2>Person Details</h2>
+      <p>Name: {{person.name}}</p>
+      <p>Age: {{person.age}}</p>
+      <p>Siblings:</p>
+      <ul v-if="person.siblings.length">
+        <li v-bind:key="sibling" v-for="sibling in person.siblings">{{sibling}}</li>
+      </ul>
+      <p>Status: {{ person.active ? 'Yes' : 'No'}}</p>
+    </div>
+
+    <strong class="msg">Person schema check passed!</strong>
+  </div>
+  
+</template>
+
+<script>
+
+export default {
+  name: 'PersonDetails',
+  props: {
+    person: Object,
+  }, 
+  methods: {
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .wrapper {
+    padding: 10px;
+    border: 1px solid #ddd;
+    width: 300px;
+    margin: 0 auto;
+  }
+  .details { 
+    margin-bottom: 30px;
+    border-bottom: 1px solid #ddd;
+  }
+  .msg {
+    color: green;
+  }
+</style>
